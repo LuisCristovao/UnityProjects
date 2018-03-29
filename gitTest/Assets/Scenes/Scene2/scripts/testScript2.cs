de574a153c06b9d8f36b03e1734046c269136914
LuisCristovao <luis.cristovao92@gmail.com>
2018-03-29 15:11:15 +0100

    test unity on git

---
+++ gitTest/Assets/Scenes/Scene2/scripts/testScript2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript2 : MonoBehaviour {
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector3(0, 15, 0));
+	}
+}
