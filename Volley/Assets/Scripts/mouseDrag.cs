8909ed9eb09d2a524d821e6cb5d7490227c1680d
LuisCristovao <luis.cristovao92@gmail.com>
2018-10-11 13:20:45 +0100

    understanding script

--- Volley/Assets/Scripts/mouseDrag.cs
+++ Volley/Assets/Scripts/mouseDrag.cs
@@ -9,12 +9,22 @@ public class mouseDrag : MonoBehaviour
 {
 
     float distance = 10;
-
+    Vector3 objectPos;
     private void OnMouseDrag()
     {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
 
         transform.position = objectPos;
+
+       // Debug.Log("MousePos:"+mousePos);
+        Debug.Log("objectPos:"+objectPos);
+        //Debug.Log("transform:"+transform.position);
+    }
+
+
+    void OnMouseUp()
+    {
+        Debug.Log("objectPos:" + objectPos);
     }
 }
