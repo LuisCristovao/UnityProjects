a47022d575d94632bc084cf011441f0594e913ce
LuisCristovao <luis.cristovao92@gmail.com>
2018-10-15 14:01:27 +0100

    ball goes back to original place

--- Volley/Assets/Scripts/Ball.cs
+++ Volley/Assets/Scripts/Ball.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    Vector2 start_pos;
 	// Use this for initialization
 	void Start () {
-		
+        start_pos = transform.position; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //print(on_air);
+
+        if (col.gameObject.name == "Floor")
+        {
+            transform.position = start_pos;
+            print(transform.position);
+        }
+    }
 }
