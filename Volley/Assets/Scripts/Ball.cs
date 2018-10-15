c59c86b605030cad8e7bd7f746b564d3cd032355
LuisCristovao <luis.cristovao92@gmail.com>
2018-10-15 15:56:20 +0100

    game versdion 1

--- Volley/Assets/Scripts/Ball.cs
+++ Volley/Assets/Scripts/Ball.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
     Vector2 start_pos;
+    public Text game_result;
+    Rigidbody2D rb;
+    public int winning_matches = 10;
 	// Use this for initialization
 	void Start () {
-        start_pos = transform.position; 
+        start_pos = transform.position;
+        rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -15,15 +20,44 @@ public class Ball : MonoBehaviour {
 		
 	}
 
+    void ChangeResult(string team,int point)
+    {
+        
+        char[] delimiters = { '|' };
+        
+        string[] result = game_result.text.Split(delimiters);
+        int[] number_result = new int[] { System.Convert.ToInt32(result[0]), System.Convert.ToInt32(result[1]) };
+        if (team == "red" || team == "r")
+        {
+            game_result.text = number_result[0].ToString() + " | " + (number_result[1]+point).ToString();
+        }
+        if (team == "green"|| team=="g")
+        {
+            game_result.text = (number_result[0] + point).ToString() + " | " + (number_result[1]).ToString();
+        }
 
+    }
     void OnCollisionEnter2D(Collision2D col)
     {
         //print(on_air);
 
-        if (col.gameObject.name == "Floor")
+        if (col.gameObject.name == "team_red_floor")
         {
             transform.position = start_pos;
-            print(transform.position);
+            rb.velocity = new Vector2(0, 0);
+            ChangeResult("g", 1);
+            //print(transform.position);
         }
+
+        if (col.gameObject.name == "team_green_floor")
+        {
+            transform.position = start_pos;
+            rb.velocity = new Vector2(0, 0);
+            ChangeResult("r", 1);
+            //print(transform.position);
+        }
+
+
+
     }
 }
